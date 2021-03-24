31fc01c5deca659637a45d08d9bb2ce70c683d28
Ömer <74145862+Wid060@users.noreply.github.com>
2021-03-25 01:45:15 +0300

    Update booster.js

--- widregister/booster.js
+++ widregister/booster.js
@@ -2,12 +2,12 @@ const { MessageEmbed } = require('discord.js')
 const config = require("../ayarlar/register.json");
 exports.run = (client,message,args) => {
 if (!message.member.roles.cache.has(config.booster_role)) {
-message.react(no);
+message.react(config.no);
 return;};
 let nick = args[0];
 if(!args[0]) return message.channel.send("**Yeni adını belirt!**")
 
-widmember.setNickname(nick);
+message.author.setNickname(nick);
 message.channel.send(`**${message.author} yeni adın \`${nick}\` olarak değiştirildi!**`)
 }
 
