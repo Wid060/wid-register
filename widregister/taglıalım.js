ac0a6d506d1c199a03887105ca794a038dddf433
Ömer <74145862+Wid060@users.noreply.github.com>
2021-03-25 02:10:37 +0300

    Update taglıalım.js

--- widregister/taglıalım.js
+++ widregister/taglıalım.js
@@ -1,11 +1,10 @@
 const { MessageEmbed } = require('discord.js')
 const config = require("../ayarlar/register.json");
-const data = require("wio.db");
 const { Database } = require('wio.db');
 const kdb = new Database("isimler");
 const krank = new Database("kayıtrank");
 exports.run = (client,message,args) => {
-let taglıalım = data.fetch(`widTaglı.${message.guild.id}`)
+let taglıalım = krank.fetch(`widTaglı.${message.guild.id}`)
     if(!args[0]) {
     message.channel.send(`${client.emojis.cache.get(config.yes)} **Taglı alım sistemini aktif etmek için "taglıalım aktif" kapatmak için "taglıalım deaktif" yazmalısın!**`).then(msg => msg.delete({ timeout: 7000, reason: 'mesaj silme' }));
     message.react(config.no);
@@ -13,12 +12,12 @@ let taglıalım = data.fetch(`widTaglı.${message.guild.id}`)
     }
         if (args[0] === "aktif") {
         if(taglıalım == "Aktif") return message.channel.send(`${client.emojis.cache.get(config.no)} **Taglı alım sistemi zaten aktif!**`).then(msg => msg.delete({ timeout: 4000, reason: 'mesaj silme' }));
-        data.set(`widTaglı.${message.guild.id}`, "Aktif")
+        krank.set(`widTaglı.${message.guild.id}`, "Aktif")
         message.channel.send(`${client.emojis.cache.get(config.yes)} **Taglı alım sistemi aktif edildi!**`).then(msg => msg.delete({ timeout: 4000, reason: 'mesaj silme' }));
         return;    
         } else if (args[0] === "deaktif") {
             if(taglıalım == "Deaktif") return message.channel.send(`${client.emojis.cache.get(config.no)} **Taglı alım sistemi zaten deaktif!**`).then(msg => msg.delete({ timeout: 4000, reason: 'mesaj silme' }));
-        data.set(`widTaglı.${message.guild.id}`, "Deaktif")
+        krank.set(`widTaglı.${message.guild.id}`, "Deaktif")
         message.channel.send(`${client.emojis.cache.get(config.yes)} **Taglı alım sistemi kapatıldı!**`).then(msg => msg.delete({ timeout: 4000, reason: 'mesaj silme' }));
         return;    
         };
